8c6b9c2cfec1792cd5f0199ac70572b736c871a0
Academy <sanela26@gmail.com>
2018-01-26 18:37:52 +0100

    prvi zadatak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MoviesComponent },
+  { path: 'movies', component: MoviesComponent },
+ 
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    MoviesComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
